53a4a491a6ab82abbd5d38130dc89e81ea391c2e
lovemango23 <70843218+lovemango23@users.noreply.github.com>
2020-09-17 16:44:38 +0900

    practice_3

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	
-	printf("enter a character : ");
-	scanf("%c",&c);
+	int a, b;
 		
-	printf("the next character of %c (%d)  is %c (%d)\n", c, c, c+1,c+1);
+	printf("input two integers : ");
+	scanf("%d %d",&a, &b);	
+	
+	printf("result is %f\n", (float)a/b); 
 	
 	return 0;
 }
